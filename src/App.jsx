404264bd6308fb32ba53c429b6e11ac1e3d94c8e
Crostons <shubh1608003@gmail.com>
2023-10-14 15:09:36 +0530

    Final commits

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navigation from './components/Navigation'
 import Home from './components/Home'
 import Upload from './components/Upload'
 import Profile from './components/Profile'
-import Dashboard from './components/Dashboard'
 
 
 function App() {
@@ -11,11 +10,9 @@ function App() {
   return (
     <div className='font-qus'>
       <Navigation />
-
       <Home/>
       <Upload />
       <Profile />
-      <Dashboard />
     </div>
   )
 }
